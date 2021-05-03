43a78abc7717e107dfef2d2bc602c9e40678d172
Facundo Jaton <facundojaton@alu.frp.utn.edu.ar>
2021-05-02 22:13:21 -0300

    Database Video Entity

--- app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
+++ app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
@@ -1,22 +1,6 @@
-/*
- * Copyright 2018, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package com.example.android.devbyteviewer.network
 
+import com.example.android.devbyteviewer.database.DatabaseVideo
 import com.example.android.devbyteviewer.domain.Video
 import com.squareup.moshi.JsonClass
 
@@ -26,8 +10,17 @@ import com.squareup.moshi.JsonClass
  * using them.
  */
 
-// TODO (03) Define extension function NetworkVideoHolder.asDatabaseModel(),
-// that returns an array of <DatabaseVideo>.
+fun NetworkVideoContainer.asDatabaseModel(): Array<DatabaseVideo> {
+    return videos.map { networkVideo ->
+        DatabaseVideo (
+                title = networkVideo.title,
+                description = networkVideo.description,
+                url = networkVideo.url,
+                updated = networkVideo.updated,
+                thumbnail = networkVideo.thumbnail
+        )
+    }.toTypedArray()
+}
 
 /**
  * VideoHolder holds a list of Videos.
